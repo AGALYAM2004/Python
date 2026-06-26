2a3f975d25494eaadd55b9fb4abf65eca966877a
AGALYA2004 <agalyamaheswaran@gmail.com>
2026-06-26 10:33:20 +0530

    Added Python Assignment and File_Handling,Exception_Handling and Regression Program

---
+++ File_Handling/csv_handling.py
@@ -0,0 +1,6 @@
+import csv
+with open(r"C:\Users\AnudipCOA\Desktop\Data Analyst Portfolio\Python\File_Handling\data.csv","w") as file:
+    writer=csv.writer(file)
+    writer.writerow(["Inkbottle", 56])
+    writer.writerow(["ledbulb", 34])
+                                                                                                                                                                                                                                                                                                                                                                                                                                                
